bebc52a29e7dee7c9d8ed1e544b42521b50728ea
Aliaksandr Khudnitski <alekskhudnitskiy@gmail.com>
2022-08-10 02:53:04 +0200

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.left = min
+        this.right = max
     }
 
     guess() {
-
+        return this.guessNumber = Math.round((this.left + this.right)/2)
     }
 
     lower() {
-
+        const mid = Math.round((this.left + this.right)/2)
+        this.setRange(this.left, this.guessNumber)
     }
-
+    
     greater() {
-
+        const mid = Math.round((this.left + this.right)/2)
+        this.setRange(this.guessNumber, this.right)
     }
 }
 
